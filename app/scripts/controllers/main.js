0e6f36f4ed6007a35e2d9979ac48ad796faabe86
Ari Lerner <arilerner@mac.com>
2013-12-09 02:06:34 -0800

    Updates

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,70 +5,52 @@ angular.module('bdayApp')
 
     $scope.$on('user:login', function() {
       updateFriends();
-      console.log("Working??????");
     });
-    // FB.getProfile()
-    // .then(function(profile) {
-    //   var split = profile.birthday.split('/'),
-    //       month = split[0] - 1,
-    //       day   = split[1],
-    //       year  = split[2] || new Date().getFullYear();
 
-    //   profile['birthday'] = new Date(year, month, day);
-    //   profile['formatted_birthday'] = $filter('date')(profile['birthday'], 'MMMM d');
-    //   $scope.profile = profile;
-      var offset = 0,
-          limit = 64;
+    // Set our default variables  
+    var offset = 0,
+        limit = 64;
 
-      var sanitizeRequestFeatures = function() {
-        if (offset < 0) offset = 0;
-      }
-      var updateFriends = function() {
-        console.log("updateFriends");
-        sanitizeRequestFeatures();
-        FB.getFriends({
-          fields: 'name,birthday,id,picture,location',
-          limit: limit,
-          offset: offset
-        })
-        .then(function(friends) {
-          var arr = []
-          angular.forEach(friends, function(value, key){
-            if (value.birthday) {
-              var split = value.birthday.split('/'),
-                  month = split[0],
-                  day   = split[1],
-                  year  = split[2] || new Date().getFullYear();
-              value['birthday'] = new Date(year, month, day);
-              value['formatted_birthday'] = $filter('date')(value['birthday'], 'MMMM dd')
-              arr.push(value);
-            }
-          });
-          $scope.friends = $filter('orderBy')(arr, 'birthday');
-          $scope.loading = false;
-        })
-      }
+    // Basically just set our offset so that we
+    // don't fetch negative friends
+    var sanitizeRequestFeatures = function() {
+      if (offset < 0) offset = 0;
+    }
+    // Update our friends list on the local scope
+    var updateFriends = function() {
+      sanitizeRequestFeatures();
+      FB.getFriends({
+        fields: 'name,birthday,id,picture,location',
+        limit: limit,
+        offset: offset
+      })
+      .then(function(friends) {
+        var arr = []
+        angular.forEach(friends, function(value, key){
+          if (value.birthday) {
+            var split = value.birthday.split('/'),
+                month = split[0],
+                day   = split[1],
+                year  = split[2] || new Date().getFullYear();
+            value['birthday'] = new Date(year, month, day);
+            value['formatted_birthday'] = $filter('date')(value['birthday'], 'MMMM dd')
+            arr.push(value);
+          }
+        });
+        $scope.friends = $filter('orderBy')(arr, 'birthday');
+        $scope.loading = false;
+      })
+    }
 
+    updateFriends();
+    $scope.nextPage = function() {
+      $scope.loading = 'next';
+      offset += limit;
       updateFriends();
-      $scope.nextPage = function() {
-        $scope.loading = 'next';
-        offset += limit;
-        updateFriends();
-      }
-      $scope.prevPage = function() {
-        $scope.loading = 'prev';
-        offset -= limit;
-        updateFriends();
-      }
-      // Groupon.getDeals({
-      //   location: profile.location.name,
-      //   radius: 50
-      // }).then(function(data) {
-      //   console.log(data);
-      //   $scope.deals = data.deals;
-      // }, function(err) {
-      //   console.error(err);
-      // });
-    // });
-
+    }
+    $scope.prevPage = function() {
+      $scope.loading = 'prev';
+      offset -= limit;
+      updateFriends();
+    }
   });
