6acceaf407c6dcad1a0bf44be3a19bd6843a4dbc
Ari Lerner <arilerner@mac.com>
2013-12-10 00:04:14 -0800

    Update localstorage

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -19,43 +19,42 @@ angular.module('bdayApp')
     // Update our friends list on the local scope
     var updateFriends = function(offset) {
       var d = $q.defer();
-      sanitizeRequestFeatures();
-      FB.getFriends({
-        fields: 'name,birthday,id,picture,location',
-        limit: limit,
-        offset: offset
-      })
-      .then(function(friends) {
-        var arr = []
-        angular.forEach(friends, function(value, key){
-          if (value.birthday) {
-            var split = value.birthday.split('/'),
-                month = split[0],
-                day   = split[1],
-                year  = split[2] || new Date().getFullYear();
-            value['birthday'] = new Date(year, month, day);
-            value['formatted_birthday'] = $filter('date')(value['birthday'], 'MMMM dd')
-            arr.push(value);
-          }
-        });
-        $scope.loading = false;
-        var output = $filter('orderBy')(arr, 'birthday');
-        localStorage.setItem('friends-'+offset, JSON.stringify(output));
-        d.resolve(output);
-      })
+      var ls = localStorage.getItem('friends-'+offset);
+      if (ls) {
+        d.resolve(JSON.parse(ls));
+      } else {
+        FB.getFriends({
+          fields: 'name,birthday,id,picture,location',
+          limit: limit,
+          offset: offset
+        })
+        .then(function(friends) {
+          var arr = []
+          angular.forEach(friends, function(value, key){
+            if (value.birthday) {
+              var split = value.birthday.split('/'),
+                  month = split[0],
+                  day   = split[1],
+                  year  = split[2] || new Date().getFullYear();
+              value['birthday'] = new Date(year, month, day);
+              value['formatted_birthday'] = $filter('date')(value['birthday'], 'MMMM dd')
+              arr.push(value);
+            }
+          });
+          $scope.loading = false;
+          var output = $filter('orderBy')(arr, 'birthday');
+          localStorage.setItem('friends-'+offset, JSON.stringify(output));
+          d.resolve(output);
+        })
+      }
       return d.promise;
     }
 
-    var ls = localStorage.getItem('friends-'+currOffset);
-    if (!ls) {
-      updateFriends(currOffset)
-      .then(function(friends) {
-        $scope.friends = friends;
-      });
-    } else {
-      $scope.friends = JSON.parse(ls);
-    }
-
+    updateFriends(currOffset)
+    .then(function(friends) {
+      $scope.friends = friends;
+    });
+    
     $scope.nextPage = function() {
       $scope.loading = 'next';
       currOffset += limit;
